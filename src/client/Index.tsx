871d343c786e8860dffdb929b1b2a69d2f9c8c07
Lidcer <47243657+Lidcer@users.noreply.github.com>
2021-08-15 20:57:08 +0200

    Delete Index.tsx

--- src/client/Index.tsx
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="../shared/fix.d.ts" />
-
-import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./App";
-
-ReactDOM.render(<App />, document.getElementById("app"));
